56c6b005bcc9c3d85979903fa8c6b7c1d23b537b
Nikolay Baryshev <pra4o@mail.ru>
2025-08-30 19:11:05 +0600

    Fix crash with invalid URL

--- Sources/SwiftyMarkdown/SwiftyMarkdown.swift
+++ Sources/SwiftyMarkdown/SwiftyMarkdown.swift
@@ -572,7 +572,7 @@ extension SwiftyMarkdown {
             if let linkIdx = styles.firstIndex(of: .link), linkIdx < token.metadataStrings.count {
                 attributes[.foregroundColor] = self.link.color
                 attributes[.font] = self.font(for: line, characterOverride: .link)
-                attributes[.link] = token.metadataStrings[linkIdx] as AnyObject
+                attributes[.link] = URL(string: token.metadataStrings[linkIdx]) as AnyObject
                 
                 if underlineLinks {
                     attributes[.underlineStyle] = self.link.underlineStyle.rawValue as AnyObject
